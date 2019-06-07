0b5d216d23e03b48ef3051d85d5acd9454cfc882
Ken Chen <lbkchen@gmail.com>
2019-06-07 18:19:36 +0900

    Adds japan photos, might need to resize later to <1 MB

--- pages/projects/japan.js
+++ pages/projects/japan.js
@@ -4,8 +4,43 @@ import Gallery from "../../components/gallery";
 const photos = [
   {
     src: "/static/images/projects/japan/shinjuku-gyoenmae.jpg",
-    width: 4,
+    width: 3,
+    height: 2
+  },
+  {
+    src: "/static/images/projects/japan/arashiyama-bamboo.jpg",
+    width: 2,
     height: 3
+  },
+  {
+    src: "/static/images/projects/japan/fushimi.jpg",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "/static/images/projects/japan/kelsey-ken.jpg",
+    width: 3,
+    height: 2
+  },
+  {
+    src: "/static/images/projects/japan/osaka-bridge.jpg",
+    width: 3,
+    height: 2
+  },
+  {
+    src: "/static/images/projects/japan/otagi.jpg",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "/static/images/projects/japan/takano-fruits.jpg",
+    width: 3,
+    height: 2
+  },
+  {
+    src: "/static/images/projects/japan/hakone-waterfall.jpg",
+    width: 3,
+    height: 2
   }
 ];
 
