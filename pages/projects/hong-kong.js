55cb491a8c7b05f8c450c298a14e097e6530c6c6
Ken Chen <lbkchen@gmail.com>
2019-06-09 19:15:24 +0900

    Adds hong-kong assets and gallery page

---
+++ pages/projects/hong-kong.js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+
+import Page from "../../layouts/main";
+import Gallery from "../../components/gallery";
+
+const photos = [
+  {
+    src: "/static/images/projects/hong-kong/cityscape.jpg",
+    width: 3,
+    height: 2
+  },
+  {
+    src: "/static/images/projects/hong-kong/montane.jpg",
+    width: 2,
+    height: 3
+  },
+  {
+    src: "/static/images/projects/hong-kong/cheung-chau.jpg",
+    width: 3,
+    height: 2
+  }
+];
+
+export default () => (
+  <Page>
+    <Head>
+      <title>Hong Kong - Ken Chen</title>
+    </Head>
+
+    <div className="gallery">
+      <h2>Hong Kong</h2>
+      <Gallery photos={photos} />
+    </div>
+
+    <style jsx>{`
+      .gallery {
+        width: 600px;
+      }
+    `}</style>
+  </Page>
+);
